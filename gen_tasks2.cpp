d124c650fe44fba62356a25211d5cbe5c9598492
Ngoguey42 <ngoguey@student.42.fr>
2016-01-17 14:47:22 +0100

    gen_tast2.cpp fully implemented

--- gen_tasks2.cpp
+++ gen_tasks2.cpp
@@ -6,7 +6,7 @@
 //   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2016/01/17 11:04:12 by ngoguey           #+#    #+#             //
-//   Updated: 2016/01/17 12:57:17 by ngoguey          ###   ########.fr       //
+//   Updated: 2016/01/17 14:46:20 by ngoguey          ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -73,11 +73,69 @@ struct Piece
 	}
 };
 
-namespace lol {
-typedef std::unordered_set<unsigned int> shapehset_t;
-typedef std::unordered_map<unsigned int, Piece> uidhmap_t;
-typedef std::multimap<unsigned int, unsigned int> shapemmap_t;
-}
+struct ValidPiecesGenerator
+{
+public:
+	typedef std::unordered_set<unsigned int> shapehset_t;
+	typedef std::unordered_map<unsigned int, Piece> uidhmap_t;
+	typedef std::multimap<unsigned int, unsigned int> shapemmap_t;
+
+	ValidPiecesGenerator() : _shapesHSet{}, _uidsHMap{}, _shapesMMap{} {}
+	~ValidPiecesGenerator() {}
+
+	ValidPiecesGenerator(ValidPiecesGenerator const &src) = delete;
+	ValidPiecesGenerator(ValidPiecesGenerator &&src) = delete;
+	ValidPiecesGenerator &operator=(ValidPiecesGenerator const &rhs) = delete;
+	ValidPiecesGenerator &operator=(ValidPiecesGenerator &&rhs) = delete;
+
+	auto operator ()(void)
+		-> std::tuple<shapehset_t, uidhmap_t, shapemmap_t> {
+
+		Piece p = {"....", "....", "....", "...."};
+
+		_gen(p, 0, 0);
+		return {std::move(_shapesHSet), std::move(_uidsHMap)
+				, std::move(_shapesMMap)};
+	}
+
+private:
+	shapehset_t _shapesHSet;
+	uidhmap_t _uidsHMap;
+	shapemmap_t _shapesMMap;
+
+	void _savePiece (Piece const &pc) {
+
+		unsigned int const shape = pc.shape();
+		unsigned int const uid = pc.uid();
+
+		_shapesHSet.insert(shape);
+		_uidsHMap.insert({uid, pc});
+		_shapesMMap.insert({shape, uid});
+		return ;
+	}
+
+	void _gen(Piece &p, int count, int istart) {
+
+		int y;
+		int x;
+
+		if (count < 4)
+		{
+			for (int i = istart; i < 16; i++)
+			{
+				y = i / 4;
+				x = i % 4;
+				p.val[y][x] = '#';
+				_gen(p, count + 1, i + 1);
+				p.val[y][x] = '.';
+			}
+		}
+		else if (p.adjDiff() >= 3)
+			_savePiece(p);
+		return ;
+	}
+};
+
 
 class ComboGenerator
 {
@@ -87,13 +145,13 @@ private:
 	static constexpr unsigned int numValidUid = 113;
 	unsigned int const _pc_count;
 
-	using shapehset_t = lol::shapehset_t;
+	using shapehset_t = ValidPiecesGenerator::shapehset_t;
 	shapehset_t const _shapesHSet;/* 19 (Piece.shape) */
 
-	using uidhmap_t = lol::uidhmap_t;
+	using uidhmap_t = ValidPiecesGenerator::uidhmap_t;
 	uidhmap_t const _uidsHMap;/* 113 (Piece.uid * Piece) */
 
-	using shapemmap_t = lol::shapemmap_t;
+	using shapemmap_t = ValidPiecesGenerator::shapemmap_t;
 	shapemmap_t const _shapesMMap;/* 113 (Piece.shape * Piece.uid) */
 
 	typedef std::vector<shapemmap_t::const_iterator> uidcombo_t;
@@ -106,13 +164,15 @@ private:
 			return 0;
 		}
 	};
+public:
 	typedef std::unordered_set<uidcombo_t, HashCombo> combohset_t;
+private:
 	combohset_t _combosHSet;
 
 public:
 	/* CONSTRUCTION ***************** */
-	ComboGenerator(unsigned int pc_count, shapehset_t shapesHSet
-			   , uidhmap_t uidsHMap, shapemmap_t shapesMMap)
+	ComboGenerator(unsigned int pc_count, shapehset_t &&shapesHSet
+				   , uidhmap_t &&uidsHMap, shapemmap_t &&shapesMMap)
 		: _pc_count(pc_count), _shapesHSet(shapesHSet)
 		, _uidsHMap(uidsHMap), _shapesMMap(shapesMMap) {
 
@@ -211,15 +271,10 @@ public:
 	void gen(unsigned int size) {
 
 		size = UI(std::min(D(size), std::pow(D(numValidUid), D(_pc_count))));
+		_combosHSet.clear();
 		_combosHSet.reserve(size);
 		for (int i = 0; i < size; i++)
 			_combosHSet.insert(_randomCombo());
-		return ;
-	}
-
-	void clear(void) {
-
-		_combosHSet.clear();
 		return ;
 	}
 
@@ -232,14 +287,115 @@ public:
 		return _combosHSet.end();
 	}
 
-	std::vector<Piece const *> operator * (combohset_t::const_iterator const &it) {
+	void convertCombo(combohset_t::const_iterator const &it
+					  , std::vector<Piece const *> &vec) const {
 
-		std::vector<Piece const *> ret;
 		uidcombo_t const &combo = *it;
+		int i;
 
-		ret.reserve(_pc_count);
+		i = 0;
+		assert(vec.size() == _pc_count);
+		vec.reserve(_pc_count);
 		for (auto const &uidIt : combo)
-			ret.push_back(&_uidsHMap.at(uidIt->second));
-		return ret;
+			vec[i++] = &_uidsHMap.at(uidIt->second);
+		return ;
 	}
 };
+
+char const b62[] = "0123456789"
+				  "abcdefghijklmnopqrstuvwxyz"
+				  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+static std::string itostrb62(unsigned int i)
+{
+	std::string str;
+
+	if (i == 0)
+		return "0";
+	while (i != 0)
+	{
+		str.insert(str.begin(), b62[i % 62]);
+		i /= 62;
+	}
+	return str;
+}
+
+static std::string filename_of_pcvec(std::vector<Piece const*> const &pcvec)
+{
+	std::string ret("./map/");
+
+	for (auto const &p : pcvec)
+	{
+		ret.append(itostrb62(p->uid()));
+		ret.append("_");
+	}
+	if (ret.size() > 200)
+	{
+		ret.erase(200);
+		ret.append("...");
+	}
+	ret.append(".fillit");
+	return ret;
+}
+
+
+static auto build_files_and_tests(
+	ComboGenerator &cg, char const *const av[], int const ntests)
+	-> std::vector<UnitTest>
+{
+	std::vector<UnitTest> tasks;
+	std::vector<Piece const *> pcvec(ntests);
+	auto it = cg.begin();
+	auto const ite = cg.end();
+	std::string fname;
+	std::ofstream f;
+
+	while (it != ite)
+	{
+		cg.convertCombo(it, pcvec);
+		fname = filename_of_pcvec(pcvec);
+		f.open(fname, std::ios_base::out | std::ios_base::trunc);
+		assert(f.good()); /* file creation failed */
+		for (auto const &p : pcvec)
+		{
+			if (&p != &*pcvec.begin()) /* funny line, but it works!!! */
+				f << '\n';
+			p->dump(f);
+		}
+		assert(f.good()); /* file write failed */
+		f.close();
+		assert(f.good()); /* file close failed */
+		tasks.push_back(UnitTest(av[3], fname));
+		tasks.push_back(UnitTest(av[4], fname));
+		it++;
+	}
+	return std::move(tasks);
+}
+
+#define MVGET(TUP, I) std::move(std::get<I>(TUP))
+std::vector<UnitTest> build_tasks(char const *const av[])
+{
+	int const pptest = std::atoi(av[1]);
+	int const ntests = std::atoi(av[2]);
+	auto pcs = ValidPiecesGenerator()();
+	ComboGenerator cg(pptest, MVGET(pcs, 0), MVGET(pcs, 1), MVGET(pcs, 2));
+	int err;
+
+	if (pptest < 0)
+	{
+		std::cerr << "Bad grids per tests " << pptest << std::endl;
+		::exit(1);
+	}
+	if (ntests <= 0)
+	{
+		std::cerr << "Bad num tests " << ntests << std::endl;
+		::exit(1);
+	}
+	std::cout << "\rRemoving ./map ./log                      ";
+	err = ::system("rm -rf map log; mkdir -p log map; false");
+	assert(err >= 0);
+	std::cout << "\rGenerating combinations                   ";
+	cg.gen(ntests);
+	std::cout << "\rGenerating map files                      ";
+	return build_files_and_tests(cg, av, ntests);
+}
