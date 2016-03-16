6c17e993aef99a1a2b8002ebbf2d82ba38ee70d1
Ngoguey42 <ngoguey@student.42.fr>
2016-03-16 13:48:49 +0100

    Add test.sh

---
+++ test.sh
@@ -0,0 +1,18 @@
+./a.out 1 200 leg/fillit ../angagnie2/fillit
+./a.out 2 200 leg/fillit ../angagnie2/fillit
+./a.out 3 200 leg/fillit ../angagnie2/fillit
+
+./a.out 9 20 leg/fillit ../angagnie2/fillit
+./a.out 10 200 leg/fillit ../angagnie2/fillit
+
+./a.out 12 20 leg/fillit ../angagnie2/fillit
+./a.out 13 200 leg/fillit ../angagnie2/fillit
+
+./a.out 16 20 leg/fillit ../angagnie2/fillit
+./a.out 17 200 leg/fillit ../angagnie2/fillit
+
+./a.out 20 20 leg/fillit ../angagnie2/fillit
+./a.out 21 200 leg/fillit ../angagnie2/fillit
+
+./a.out 25 20 leg/fillit ../angagnie2/fillit
+./a.out 26 200 leg/fillit ../angagnie2/fillit
